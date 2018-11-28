40a6c3d685e7a75c711fc8bf26350053fff0189e
robince <robince885@gmail.com>
2018-11-28 19:25:24 +0100

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,55 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { HealthController } from "./health.controller";
+import { AppService } from "./app.service";
+import { StatusMonitorModule } from "nest-status-monitor";
+
+const statusMonitorConfig = {
+  pageTitle: "Nest.js Status Monitor", // Default title
+  path: "/status",
+  port: 3000,
+  spans: [
+    {
+      interval: 1, // Every second
+      retention: 60 // Keep 60 datapoints in memory
+    },
+    {
+      interval: 5, // Every 5 seconds
+      retention: 60
+    },
+    {
+      interval: 15, // Every 15 seconds
+      retention: 60
+    }
+  ],
+  healthChecks: [
+    {
+      protocol: "http",
+      host: "localhost",
+      path: "/health/alive",
+      port: 3000
+    },
+    {
+      protocol: "http",
+      host: "localhost",
+      path: "/health/dead",
+      port: 3000
+    }
+  ],
+  chartVisibility: {
+    cpu: true,
+    mem: true,
+    load: true,
+    responseTime: true,
+    rps: true,
+    statusCodes: true
+  },
+  ignoreStartsWith: "/health/alive"
+};
+
+@Module({
+  imports: [StatusMonitorModule.setUp(statusMonitorConfig)],
+  controllers: [AppController, HealthController],
+  providers: [AppService]
+})
+export class AppModule {}
